3bdf79a882ec0bdf0aa00f29f045656c55c62298
Holger Harms <hello@holhar.de>
2024-09-05 16:01:10 +0200

    Data Fetching

---
+++ app/components/project-list.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Projects</div>
+
+    <section v-if="status === 'pending'">Loading...</section>
+    <section v-else-if="error">Something went wrong... Try again!</section>
+    <section v-else>Here we display the repos!</section>
+</template>
+
+<script setup>
+const { error, status, data } = await useFetch('https://api.github.com/users/holhar/repos')
+</script>
